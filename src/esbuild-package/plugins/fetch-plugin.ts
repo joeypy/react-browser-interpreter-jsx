2b1b10d8645a9b7a2672a1d9bda8e13890f9ea47
Joseph Boscan <joeypc.py@gmail.com>
2022-11-27 11:53:53 -0400

    Making refactor in onLoad fetch plugin

--- src/esbuild-package/plugins/fetch-plugin.ts
+++ src/esbuild-package/plugins/fetch-plugin.ts
@@ -27,29 +27,35 @@ export const fetchPlugin = (inputCode: string) => {
         if (cachedResult) {
           return cachedResult;
         }
+      });
 
+      build.onLoad({ filter: /.css$/ }, async (args: any) => {
         const { data, request } = await axios.get(args.path);
-
-        const fileType = args.path.match(/.css$/) ? 'css' : 'jsx';
-
         const escaped = data
           .replace(/\n/g, '')
           .replace(/"/g, '\\"')
           .replace(/'/g, "\\'");
-        const contents =
-          fileType === 'css'
-            ? `
+        const contents = `
                 const style = document.createElement('style');
                 style.innerText = '${escaped}';
                 document.head.appendChild(style);
-              `
-            : data;
-
-        // import 'bulma/css/bulma.css'
+              `;
 
         const result: esbuild.OnLoadResult = {
           loader: 'jsx',
           contents,
+          resolveDir: new URL('./', request.responseURL).pathname,
+        };
+        await fileCache.setItem(args.path, result);
+        return result;
+      });
+
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        const { data, request } = await axios.get(args.path);
+
+        const result: esbuild.OnLoadResult = {
+          loader: 'jsx',
+          contents: data,
           resolveDir: new URL('./', request.responseURL).pathname,
         };
         // store response in cache
