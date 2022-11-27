1f9fb1f424e1bcbcd56290dac6afd0d5b7821f27
Joseph Boscan <joeypc.py@gmail.com>
2022-11-27 08:26:59 -0400

    Fixing error importing from subdirectories

--- src/esbuild-package/plugins/unpkg-path-plugin.ts
+++ src/esbuild-package/plugins/unpkg-path-plugin.ts
@@ -11,12 +11,24 @@ export const unpkgPathPlugin = () => {
         if (args.path === "index.js") {
           return { path: args.path, namespace: "a" };
         }
-        else if (args.path === "tiny-test-pkg") {
+
+        if (args.path.includes("./") || args.path.includes("../")) {
           return {
-            path: "https://www.unpkg.com/tiny-test-pkg@1.0.0/index.js",
             namespace: "a",
+            path: new URL(args.path, args.importer + "/").href,
           };
         }
+
+        return {
+          path: `https://www.unpkg.com/${args.path}`,
+          namespace: "a",
+        };
+        // else if (args.path === "tiny-test-pkg") {
+        //   return {
+        //     path: "https://www.unpkg.com/tiny-test-pkg@1.0.0/index.js",
+        //     namespace: "a",
+        //   };
+        // }
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
@@ -26,7 +38,7 @@ export const unpkgPathPlugin = () => {
           return {
             loader: "jsx",
             contents: `
-              import message from 'tiny-test-pkg';
+              import message from 'nested-test-pkg';
               console.log(message);
             `,
           };
